2d6cef171750c4efc44bfeeb03b00f71681ab8ca
Son Nguyen <s4052257@rmit.edu.vn>
2024-01-16 10:42:24 +0700

    TestPage

--- climate-change.project/src/main/java/com/example/climatechange/project/TestPage.java
+++ climate-change.project/src/main/java/com/example/climatechange/project/TestPage.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class test{
+public class TestPage{
     @RequestMapping("/")
     @ResponseBody
 	public String indexPage() {
